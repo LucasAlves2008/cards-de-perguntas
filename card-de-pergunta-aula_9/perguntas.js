e4963922c7592669536dde235e029bcda2d4d8f9
LucasAlves2008 <00001105141433sp@al.educacao.sp.gov.br>
2024-11-26 22:43:49 -0300

    Add files via upload

---
+++ card-de-pergunta-aula_9/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem da cobrinha'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz olá em Inglês?',
+    'Hello'
+)
